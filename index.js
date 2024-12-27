3b4989473414acc75168ab185ccaadd27b6c3e3c
TAIJULAMAN <taijulaman8@gmail.com>
2024-12-27 15:05:34 +0600

    mega mert server

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rms22hp.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const dealCollection = client.db("SodiumCafe").collection("bestDeals");
+    // create a new deal
+    app.get("/bestDeals", async (req, res) => {
+      const cursor = dealCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection...............................
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// check check check
+app.get("/", (req, res) => {
+  res.send("sodium cafe server is running.");
+});
+app.listen(port, () => {
+  console.log(`Server is runnung on port: ${port}`);
+});
